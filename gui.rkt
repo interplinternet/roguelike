64b7b7fe6a545dc89154e263f53973deca99dbde
interplinternet <astro@airmail.cc>
2016-07-24 16:48:47 -0700

    Factor main% out of GUI module, add grid.
    main% is now it's own module, and will be where the meat of the project
    is located. I've added deep-map, which is used to update a grid, which
    is currently a nested list. Added an example of the hash-table I might
    be using to hold entites in the game, where each monster is hashed by
    its posn, and adding monsters conses the monster onto the list of other
    monsters hashed to that key.

--- gui.rkt
+++ gui.rkt
@@ -1,47 +1,7 @@
 #lang racket/gui
 (require pict "main.rkt")
 ;;---------------------------------------------------------------------------------------------------
-#| Constants |#
-(define UNIT 18)
-(define WIDTH 800)
-(define HEIGHT 800)
-(define UNITSPEED (/ WIDTH 100))
-(define FONT (make-font #:size UNIT #:family 'modern))
-(define EXMSGLOG ('"most recent message" "immediately prior" "last message"))
-
-(define PLAYER-IMG
-  (text "@" 'modern UNIT))
-;;----------------------------------------------------------------------------------------------------
-#| Structs |#
-(struct posn [x y] #:transparent)
-; A Posn is (posn Number Number),
-; the X- & Y-Coordinate of an entity on the level
-(struct being [hp dmg loc] #:transparent)2
-; A being is: (player Number Number Posn)
-
-(struct world [entities level] #:transparent)
-; A Game is (game [Listof Being] Grid)
-
-(struct player being [eqp inv] #:transparent)
-; A Player is (player Number Number Posn [Listof Item] [Listof Item])
-
-(struct terrain (type))
-; The terrain of a cell := (terrain String),
-; where String is "floor" or "wall"
-;;---------------------------------------------------------------------------------------------------
-#| Examples |#
-
-(define explayer (player 100 10 (posn 100 100) '(sword shield) '(bag bow arrow)))
-(define exmons '(orc goblin doggo))
-(define exgrid
-  (for*/list ([row (in-range 10)]
-              [col (in-range 10)])
-    (list (posn row col) 'nothing 'yet)))
-; A grid is the game-playing map, where each "cell" is one UNIT wide and tall and contains terrain
-; information.
-
-;;---------------------------------------------------------------------------------------------------
-#| Objects & Classes |#
+#| GUI |#
 ; Try to figure out some kind of MVC thing.
 
 #| CONTAINERS|#
@@ -72,7 +32,7 @@
 (define text-draw-mixin
   (mixin (canvas<%>) (text-draw<%>)
     (super-new)
-    
+
     ; [Listof String] -> Image
     (define/public (draw-vl-text los)
       (apply vl-append (map (curryr text 'modern UNIT) los)))
@@ -83,76 +43,47 @@
     ))
 
 #| CLASSES |#
-(define msg% (text-draw-mixin (class canvas% (super-new) (init-field [message-log EXMSGLOG]))))
+(define msg%
+  (text-draw-mixin (class canvas% (super-new)
+                     (init-field [message-log EXMSGLOG]))))
 
 ; The inventory screen, where items are listed.
 (define inv-canvas% (text-draw-mixin canvas%))
 
 ; The Main screen, where the action happens.
-(define main%
-  (class canvas%
-    (init-field [plyr explayer])
-    (init-field [monsters exmons])
-    (init-field [grid exgrid])
-
-    ; DC -> Void
-    (define/public (draw-player dc)
-      (match-define (player _ _ (posn x y) _ _) plyr)
-      (draw-pict PLAYER-IMG dc x y))
-
-    ; KeyEvent -> Void
-    (define/override (on-char event)
-      (update-game! event)
-      (send main-screen refresh))
-
-    ; KeyEvent -> Void
-    ; When we want to update an entire game, we process the sequence containing all entities,
-    ; moving them as necessary
-    (define (update-game! event)
-      (match-define (player health damage (posn x y) equipment inventory) plyr)
-      (set! plyr 
-            (player health damage (update-location event x y) equipment inventory)))
-
-    ; KeyEvent Number Number -> Posn
-    (define (update-location event x y)
-      (match (send event get-key-code)
-        ['up    (posn x (- y UNITSPEED))]
-        ['down  (posn x (+ y UNITSPEED))]
-        ['left  (posn (- x UNITSPEED) y)]
-        ['right (posn (+ x UNITSPEED) y)]
-        [_      (posn x y)]))
-    (super-new)))
+; (factored out)
 
 #| OBJECTS |#
 (define main-screen
-;How do we render a whole screen? We take a map, and for every row we step through each column,
-;using its location to pull any entities located there. We render the terrain, then we render the
-;objects, followed by monsters/player. Right now we just render the player.
+  ;How do we render a whole screen? We take a map, and for every row we step through each column,
+  ;using its location to pull any entities located there. We render the terrain, then we render the
+  ;objects, followed by monsters/player. Right now we just render the player.
   (new main%
        [parent main-pane]
        [label "Main"]
        [style '(border)]
        [min-width 400]
        [min-height 400]
+       [stretchable-width #t]
        [paint-callback
         (λ (canvas dc)
           (send main-screen draw-player dc))]))
 
 (define msg
-  ; Eventually, Health & Equipped should be factored into their own panes/canvases.
-  ; Then I can label them and wrap all rendering calls with get-label instead of having to cons
-  ; & append titles in every paint-callback function.
-  ; Options: Split the inventory pane into two panes: One for general inventory,
-  ; one for equipped items.
-  ; Or change the contents of the pane based on key presses, when "i" set it to general inventory,
-  ; When "e" set it to equipped items.
-  ; Or have all of them created, but have them hidden based on keypresses like above.
+  ; Eventually, Health & Equipped should be factored into their own panes/canvases. Then I can label
+  ; them and wrap all rendering calls with get-label instead of having to cons & append titles in
+  ; every paint-callback function. Options: Split the inventory pane into two panes: One for general
+  ; inventory, one for equipped items. Or change the contents of the pane based on key presses, when
+  ; "i" set it to general inventory, When "e" set it to equipped items. Or have all of them created,
+  ; but have them hidden based on keypresses like above.
   (new msg%
        [parent msg-panel]
        [paint-callback
         (λ (canvas dc)
           (match-define (player health _ _ equipment _)
             (get-field plyr main-screen))
+          ; maybe the main screen should have an always-displayed pane containing a msg% which
+          ; displays health?
           (define hp (string-append "Health: " (number->string health)))
           (define eqp (cons "Equipped: " (map symbol->string equipment)))
           (send msg textlist->image
@@ -162,7 +93,6 @@
 (define inv-screen
   (new inv-canvas%
        [parent inv-pane]
-       [label "Inventory"]
        [style '(border)]
        [min-width 400]
        [stretchable-width #f]
--- main.rkt
+++ main.rkt
@@ -1,2 +1,118 @@
-#lang racket
+#lang racket/gui
 (require pict)
+(provide (all-defined-out))
+;;---------------------------------------------------------------------------------------------------
+#| Constants |#
+(define UNIT 18)
+(define WIDTH 800)
+(define HEIGHT 800)
+(define UNITSPEED (/ WIDTH 100))
+(define FONT (make-font #:size UNIT #:family 'modern))
+(define EXMSGLOG '("most recent message" "immediately prior" "last message"))
+
+(define PLAYER-IMG
+  (text "@" 'modern UNIT))
+;;----------------------------------------------------------------------------------------------------
+#| Structs |#
+(struct posn [x y] #:transparent)
+; A Posn is (posn Number Number),
+; the X- & Y-Coordinate of an entity or cell on the level
+
+(struct being [hp dmg loc] #:transparent)
+; A being is: (player Number Number Posn)
+
+(struct world [entities level] #:transparent)
+; A Game is (game [Listof Being] Grid)
+
+(struct player being [eqp inv] #:transparent)
+; A Player is (player Number Number Posn [Listof Item] [Listof Item])
+
+(struct cell [location content] #:transparent)
+;A Cell is (cell Posn Any), where Posn represents its location, and Any is the content.
+
+(struct terrain (type))
+; The terrain of a cell := (terrain String),
+; where String is "floor" or "wall"
+
+#| Functions |#
+; Number Any -> [Listof [Listof Posn Any]]
+; A nested for/list call is not quite the same as for*/list. The former creates a list of lists, which
+; we want for easy reference to each row, and the latter creates a single flattened list.
+(define (make-grid len content)
+  (for/list ([row (in-range len)])
+    (for/list ([col (in-range len)])
+      (cell (posn row col) content))))
+
+; [Listof [Listof Posn Any]] -> [Listof [Listof Posn Any]]
+; A grid is a list of lists. We update by applying f to every element.
+(define (update-grid a-grid f)
+  (deep-map a-grid f))
+
+;[Listof [Listof Any]] [Any -> Any] -> [Listof [Listof Any]]
+(define (deep-map lol f)
+  (cond
+    [(empty? lol) '()]
+    [(cons? (first lol))
+     (cons (deep-map (first lol) f)
+           (deep-map (rest lol) f))]
+    [else (cons (f (first lol))
+                (deep-map (rest lol) f))]))
+
+;;---------------------------------------------------------------------------------------------------
+#| Examples |#
+
+(define explayer (player 100 10 (posn 100 100) '(sword shield) '(bag bow arrow)))
+(define exmons '(orc goblin doggo))
+(define exgrid (make-grid 3 'nada))
+(define exmonhash
+  (make-immutable-hash `([,(posn 0 0) . (orc)]
+                         [,(posn 2 3) . (goblin)]
+                         [,(posn 1 1) . (doggo)])))
+(define exmonhash2 (hash-update exmonhash (posn 0 0) (curry cons 'gobbo)))
+#| MAIN |#
+; A grid is the game-playing map, where each "cell" is one UNIT wide and tall and contains terrain
+; information.
+(define main%
+  (class canvas%
+    (init-field [plyr explayer])
+    (init-field [monsters exmons])
+    (init-field [grid exgrid])
+
+    ; DC -> Void
+    (define/public (draw-player dc)
+      (match-define (player _ _ (posn x y) _ _) plyr)
+      (draw-pict PLAYER-IMG dc x y))
+
+    ; KeyEvent -> Void
+    (define/override (on-char event)
+      (update-game! event)
+      (send this refresh)) ; (send main-screen refresh)
+
+    ; KeyEvent -> Void
+    ; When we want to update an entire game, we process the sequence containing all entities,
+    ; moving them as necessary
+    (define (update-game! event)
+      (match-define (player health damage (posn x y) equipment inventory) plyr)
+      (set! plyr
+            (player health damage (update-location event x y) equipment inventory)))
+
+    ; KeyEvent Number Number -> Posn
+    (define (update-location event x y)
+      (match (send event get-key-code)
+        ['up    (posn x (- y UNITSPEED))]
+        ['down  (posn x (+ y UNITSPEED))]
+        ['left  (posn (- x UNITSPEED) y)]
+        ['right (posn (+ x UNITSPEED) y)]
+        [_      (posn x y)]))
+    (super-new)))
+
+(define dummy-frame
+  (new frame%
+       [label "dummy"]
+       [min-width 800]
+       [min-height 800]))
+(define dummy (new main%
+                   [parent dummy-frame]
+                   [paint-callback
+                    (λ (canvas dc)
+                      (send dummy draw-player dc))]))
