822a6373f1aa3ff22e4a822a8033d7d58462a7b7
interplinternet <astro@airmail.cc>
2016-07-24 23:24:15 -0700

    Add: whats-on, item/monster hash-tables.
    Whats-on takes a posn and returns whats on that posn in the world.
    Monsters and items are stored in a hash-table where their posn is the
    key.

--- main.rkt
+++ main.rkt
@@ -8,7 +8,7 @@
 (define HEIGHT 800)
 (define UNITSPEED (/ WIDTH 100))
 (define FONT (make-font #:size UNIT #:family 'modern))
-(define EXMSGLOG '("most recent message" "immediately prior" "last message"))
+
 
 (define PLAYER-IMG
   (text "@" 'modern UNIT))
@@ -18,11 +18,12 @@
 ; A Posn is (posn Number Number),
 ; the X- & Y-Coordinate of an entity or cell on the level
 
+; Maybe I should implement some generics for these?
 (struct being [hp dmg loc] #:transparent)
 ; A being is: (player Number Number Posn)
 
-(struct world [entities level] #:transparent)
-; A Game is (game [Listof Being] Grid)
+(struct world [entities items level] #:transparent)
+; A Game is (game [Listof Being] [Listof Item] Grid)
 
 (struct player being [eqp inv] #:transparent)
 ; A Player is (player Number Number Posn [Listof Item] [Listof Item])
@@ -34,6 +35,7 @@
 ; The terrain of a cell := (terrain String),
 ; where String is "floor" or "wall"
 
+;;--------------------------------------------------------------------------------------------------
 #| Functions |#
 ; Number Any -> [Listof [Listof Posn Any]]
 ; A nested for/list call is not quite the same as for*/list. The former creates a list of lists, which
@@ -58,17 +60,37 @@
     [else (cons (f (first lol))
                 (deep-map (rest lol) f))]))
 
+; Posn Level -> [List [Listof Being] [Listof Item]]
+(define (whats-on pos a-level)
+  (define item-table (world-items a-level))
+  (define entity-table (world-entities a-level))
+  (cond
+    [(and (hash-has-key? entity-table pos) (hash-has-key? item-table pos))
+     (list (hash-ref entity-table pos) (hash-ref item-table pos))]
+    [(and (hash-has-key? entity-table pos) (not (hash-has-key? item-table pos)))
+     (list (hash-ref entity-table pos))]
+    [(and (hash-has-key? item-table pos) (not (hash-has-key? entity-table pos)))
+     (list (hash-ref item-table pos))]
+    [else #f]))
+
 ;;---------------------------------------------------------------------------------------------------
 #| Examples |#
 
 (define explayer (player 100 10 (posn 100 100) '(sword shield) '(bag bow arrow)))
 (define exmons '(orc goblin doggo))
-(define exgrid (make-grid 3 'nada))
+(define exgrid (make-grid 3 (terrain "floor")))
+(define EXMSGLOG '("most recent message" "immediately prior" "last message"))
 (define exmonhash
   (make-immutable-hash `([,(posn 0 0) . (orc)]
                          [,(posn 2 3) . (goblin)]
+                         [,(posn 3 3) . (,explayer)]
                          [,(posn 1 1) . (doggo)])))
 (define exmonhash2 (hash-update exmonhash (posn 0 0) (curry cons 'gobbo)))
+(define exitemhash
+  (make-immutable-hash `([,(posn 0 0) . (chest)]
+                         [,(posn 2 3) . (dagger potion)]
+                         [,(posn 3 3) . (arrow)])))
+(define exworld (world exmonhash exitemhash exgrid))
 #| MAIN |#
 ; A grid is the game-playing map, where each "cell" is one UNIT wide and tall and contains terrain
 ; information.
