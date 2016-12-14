422b72ed57af5b451ee3631e4b0518cadf2712b0
interplinternet <astro@airmail.cc>
2016-12-13 17:40:30 -0800

    Fixes hallway connecting function hanging.
    * Prior to this the function would randomly hang, I think because it would
      alternate back and forth between cells it had already traversed as none of
      them were closer than the other. I removed the already traversed cells from
      the grid of potential choices. Then it would violate argmin's contract because
      the surrounding list of cells to choose from is empty. In that case, return a
      random orthogonal cell. It works, but creates bizarre cavernous "paths" that
      can take up half of a map. This still needs works, but should no longer hang.

--- Level/level.rkt
+++ Level/level.rkt
@@ -22,7 +22,7 @@
      (define new-room (make-room (room-name first-room)))
      (list* new-room
             (add-new-neighbor first-room (room-name new-room))
-            (list (rest list-of-room)))]
+            (rest list-of-room))]
     [else list-of-room]))
 
 ; Room -> Boolean
@@ -55,127 +55,111 @@
   (circle/g (posn 10 10) 2))
 (define circle-level (list (room 'alpha excircle (posn 6 6) '(beta))
                            (room 'beta excircle2 (posn 10 10) '(alpha))))
-; Level -> [List Grid Grid]
-; Consumes a representation of a level, returns two grids: one containing all "empty" cells, and one
-; containing all "wall" cells, or those which are not in a room.
-(define (make-grid2 level)
-  (define initial-grid (blank-grid WIDTH HEIGHT))
-  ; - IN -
-  (partition (λ (a-cell)
-               (for/or ([a-room level])
-                 (cell-fits? a-cell a-room)))
-             initial-grid))
-
-; Level -> [List Grid Grid]
-; Consumes a level representation and returns 2 grids: one containing floor cells and one containing
-; wall cells. Take all the cells in the grid that are in the first room, and connect them to the last
-; existing room and then recurse.
-(define (make-grid3 level0)
-  (define initial-grid (blank-grid WIDTH HEIGHT))
-  (define start (first level0))
-  (define seen '())
-  ; - IN -
-  (walk-and-connect start initial-grid (cons start seen)))
-
 (define level? (listof room?))
 (define grid? (listof cell?))
 
 ; Level -> Grid
+; Sometimes this stalls indefinitely, why?
 (define/contract (make-grid level0)
   (level? . -> . grid?)
   (define initial-grid (blank-grid WIDTH HEIGHT))
   (define start-room (first level0))
-  (walk-and-connect start-room level0 initial-grid '()))
+  (remove-duplicates (walk-and-connect start-room level0 initial-grid (list (room-name start-room)))))
 
 (define room-or-empty? (or/c room? empty?))
 (define room-name? symbol?)
 
 ; Room [Listof Room] [Listof Cell] [Listof Symbol] -> [Listof Cell]
-(define (walk-and-connect home-room level walls seen)
+(define/contract (walk-and-connect home-room level walls seen)
+  (room? level? grid? (listof symbol?) . -> . (non-empty-listof cell?))
   (define home-grid (punch-through home-room walls)) ;[Listof Cell]
-  (define neighbors (room-neighbors home-room)) ; [Listof Symbol]
-  (define hallway (connect home-grid (select-random home-grid)
-                           (select-room (first neighbors) level) walls))
-  (append home-grid
-          hallway
-          (connect-neighbors neighbors level walls (cons (room-name home-room) seen))))
+  (define neighbors0 (room-neighbors home-room)) ; [Listof Symbol]
+  (define neighbors (remove* seen neighbors0))
+  (cond
+    [(empty? neighbors) home-grid]
+    [else
+     (define hallway (connect home-grid (select-random home-grid)
+                              (select-room (first neighbors) level) walls))
+     (append home-grid
+             hallway
+             (connect-neighbors neighbors level walls (cons (room-name home-room) seen)))]))
 
 ; [Listof Symbol] [Listof Room] [Listof Cell] [Listof Symbol] -> [Listof Cell]
-(define (connect-neighbors lon0 level walls seen)
-  (define lon (remove* seen lon0))
+(define/contract (connect-neighbors lon level walls seen)
+  ((listof symbol?) level? grid? (listof symbol?) . -> . (non-empty-listof cell?))
   (cond
     [(empty? lon) '()]
     [else
-     (define seen-now (cons (first lon) seen))
+     (define seen-now seen)
      (append (walk-and-connect (select-room (first lon) level) level walls seen-now)
              (connect-neighbors (rest lon) level walls seen-now))]))
+
 ; Grid Cell Room  Grid -> Grid
 ; Connects a grid representing one room to one neighbor and returns a new grid representing the
 ; hallway between them.
 (define/contract (connect room-grid closest-cell neighbor grid)
-  ((listof cell?) cell? room? (listof cell?) . -> . (listof cell?))
+  ((listof cell?) cell? room? (listof cell?) . -> . (non-empty-listof cell?))
   (define neighbor-grid (punch-through neighbor grid))
 
+  ; Cell [Listof Cell] -> [Listof Cell]
   (define (connect/helper close-cell cell-path)
     (cond
       [(inside? close-cell neighbor-grid)
        (cons close-cell cell-path)]
-      [else (define new-closest-cell (pick-closest close-cell neighbor-grid grid))
+      [else (define new-closest-cell
+              (pick-closest close-cell neighbor-grid cell-path grid))
             (connect/helper new-closest-cell (cons close-cell cell-path))]))
   ; - IN -
   (connect/helper closest-cell '()))
 
-
-
-
 ; Room Grid -> Grid
 (define/contract (punch-through a-room grid)
-  (room? (listof cell?) . -> . (listof cell?))
+  (room? (listof cell?) . -> . (non-empty-listof cell?))
   (filter (λ (a-cell) (cell-fits? a-cell a-room)) grid))
 
 ; Cell Room -> Boolean
 (define/contract (cell-fits? a-cell a-room)
   (cell? room? . -> . boolean?)
-  (define the-shape (room-function a-room))
-  (the-shape (cell-anchor a-cell)))
+  ((room-function a-room) (cell-anchor a-cell)))
 
 ; Cell Grid -> Boolean
-(define/contract (inside? a-cell grid)
-  (cell? (listof cell?) . -> . boolean?)
-  (and (member a-cell grid) #t))
+(define (inside? a-cell grid)
+  (member a-cell grid))
 
 ; Cell Grid Grid -> Cell
-(define (pick-closest a-cell target-grid grid)
+(define/contract (pick-closest a-cell target-grid cell-path grid0)
+  (cell? (non-empty-listof cell?) (listof cell?) (non-empty-listof cell?) . -> . cell?)
   (define target (select-random target-grid))
   (define target-sum (sum-coords target))
-  (define surroundings (orthogonal-cells a-cell))
-  (argmin (λ (a-cell) (abs (- (sum-coords a-cell) target-sum))) surroundings))
+  (define grid (remove* cell-path grid0))
+  ; if there are no orthogonal cells, then return a random orthogonal cell to prevent hanging.
+  (define surroundings (orthogonal-cells a-cell grid))
+  (if (empty? surroundings) (select-random (orthogonal-cells a-cell grid0))
+      (argmin (λ (a-cell) (abs (- (sum-coords a-cell) target-sum))) surroundings)))
 
 ; Cell -> Number
-(define (sum-coords a-cell)
-  (match a-cell
-    [(cell (posn x y)) (+ x y)]))
+(define/match (sum-coords a-cell)
+  [((cell (posn x y))) (+ x y)])
 
 ; Cell -> Grid
-(define/contract (orthogonal-cells a-cell)
-  (cell? . -> . (listof cell?))
+(define/contract (orthogonal-cells a-cell grid)
+  (cell? (listof cell?) . -> . (listof cell?))
   (match-define (cell (posn x y)) a-cell)
-  ; it will be better to select cells by their coordinate, but for now since cells contain
-  ; no information it's fine to just "recreate" them
-  (map (λ (pos) (cell pos))
-       (filter-not (λ (pos) (or (negative? (posn-x pos)) (negative? (posn-y pos))))
-                   (list (posn (sub1 x) (sub1 y)) ; upper-left
-                         (posn x (sub1 y)) ; above
-                         (posn (add1 x) (sub1 y)) ; upper-right
-                         (posn (sub1 x) y) ; left
-                         (posn (add1 x) y) ; right
-                         (posn (sub1 x) (add1 y)) ; lower-left
-                         (posn x (add1 y)) ; below
-                         (posn (add1 x) (add1 y)))))); lower-right
+
+  (define orthogonal-c
+    (map (curry cell)
+         (list (posn x (sub1 y)) ; above
+               (posn (sub1 x) y) ; left
+               (posn (add1 x) y) ; right
+               (posn x (add1 y))))); below
+
+  (for/list ([c grid]
+             #:when (member c orthogonal-c))
+    c))
 
 ; Number Number -> Grid
 ; Consumes 2 numbers representing the number of logical cells each row and column should have.
-(define (blank-grid w h) 
+(define (blank-grid w h)
   (for*/list ([y (in-range h)]
               [x (in-range w)])
     (cell (posn x y))))
@@ -189,8 +173,7 @@
 ; Number -> Level
 ; Generates a level containing Number amount of rooms.
 (define (make-level number-of-rooms)
-  (self-apply new-room (list (room (gensym) (random-shape) BLANK-POSN empty-neighborhood))
-              number-of-rooms))
+  (self-apply new-room '() number-of-rooms))
 
 ;;---------------------------------------------------------------------------------------------------
 #| Rendering |#
@@ -235,11 +218,17 @@
 ; Grid Grid -> image
 ; Consumes two grids, one of cells which are located in rooms, and one which is not.
 (define (draw-grid a-level)
-  (define-values (floor wall)
-    (make-grid a-level))
+  (define floor (make-grid a-level))
+  (define wall (remove* floor (blank-grid WIDTH HEIGHT)))
   ; - IN -
   (lt-superimpose (draw-cells (const black-cell) wall)
                   (draw-cells (λ (a-cell) (pin-cell-coords white-cell a-cell)) floor)))
+
+(define (draw-grid2 a-level)
+  (define grid (make-grid a-level))
+  ; - IN -
+  (lt-superimpose (draw-cells (λ (a-cell) (pin-cell-coords white-cell a-cell)) grid)
+                  (draw-cells (const black-cell) (blank-grid WIDTH HEIGHT))))
 
 ; Grid -> Image
 (define (draw-cells colored-cell grid)
