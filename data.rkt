f532a5d806fe1e898757141672ca2000eb16e1dd
interplinternet <astro@airmail.cc>
2016-12-15 12:41:42 -0800

    Re-add data and helper modules.

---
+++ data.rkt
@@ -0,0 +1,38 @@
+#lang racket
+(provide WIDTH HEIGHT PWIDTH PHEIGHT ROOM-WIDTH ROOM-HEIGHT CELL NEIGHBOR-MAX
+         (contract-out [struct cell ((anchor posn?) (terrain symbol?))])
+         (contract-out [struct posn ((x number?) (y number?))])
+         (contract-out [struct room ((name symbol?) (function (-> posn? any/c)) (anchor posn?)
+                                     (neighbors (listof symbol?)))]) 
+         )
+;;---------------------------------------------------------------------------------------------------
+#| CONSTANTS |#
+(define WIDTH 100) ; the width of a level is 100 logical cells
+(define HEIGHT WIDTH)
+(define PWIDTH 800) ;physical width and height, i.e., in pixels.
+(define PHEIGHT PWIDTH)
+(define ROOM-WIDTH (round (/ WIDTH 4))) ; a room can be no larger than a quarter of the map
+(define ROOM-HEIGHT ROOM-WIDTH)
+(define CELL (round (/ PWIDTH WIDTH))) ; a cell is wide enough to fid WIDTH number of cells in PWIDTH
+(define NEIGHBOR-MAX 4)
+
+;;---------------------------------------------------------------------------------------------------
+#| Data |#
+; A Level is [List Room Room Room Room] or [Empty]
+; A Room is (room Symbol [X -> Y] Posn Level) such that level has no more than 4 rooms.
+
+; Grid := [Listof Cell]
+; Cell := (cell Posn Terrain)
+; Where Posn is the anchor of the cell, in the upper left corner, and terrain is a symbol.
+(struct cell (anchor terrain) #:transparent) 
+
+; A room is:
+; (struct [Number -> Any] Number)
+(struct posn [x y] #:transparent)
+
+; A Room is:
+(struct room [name function anchor neighbors] #:transparent)
+; (room Symbol [Posn -> Any] Posn [Listof Symbol])
+; The function determines the shape of the room, and
+; the anchor is the location of its central point in the level. Neighbors field is a list of symbols
+; representing the names of all of its neighbors. -- A room is really a node in a graph.
